8e7fed30d8c1e2fe8f355bbfd1320efb22101e2d
curtis <curtis@fillygaming888.com>
2023-02-01 16:57:06 +0800

    Doc readme

--- README.md
+++ README.md
@@ -11,3 +11,59 @@ main(){
 }
 ```
 
+## 支援的 codec
+```dart
+class AnsiCodecs implements AnsiCodecSketch {
+  final AnsiCodecSketch codec;
+
+  AnsiCodecs._(this.codec);
+
+  factory AnsiCodecs.cp950() {
+    return AnsiCodecs._(const CP950Codec());
+  }
+
+  factory AnsiCodecs.cp949() {
+    return AnsiCodecs._(const CP949Codec());
+  }
+
+  factory AnsiCodecs.cp936() {
+    return AnsiCodecs._(const CP936Codec());
+  }
+
+  factory AnsiCodecs.cp932() {
+    return AnsiCodecs._(const CP932Codec());
+  }
+
+  factory AnsiCodecs.cp874() {
+    return AnsiCodecs._(const CP874Codec());
+  }
+
+  factory AnsiCodecs.cp1250() {
+    return AnsiCodecs._(const CP1250Codec());
+  }
+
+  factory AnsiCodecs.cp1251() {
+    return AnsiCodecs._(const CP1251Codec());
+  }
+
+  factory AnsiCodecs.cp1252() {
+    return AnsiCodecs._(const CP1252Codec());
+  }
+
+  factory AnsiCodecs.cp1253() {
+    return AnsiCodecs._(const CP1253Codec());
+  }
+
+  factory AnsiCodecs.cp1254() {
+    return AnsiCodecs._(const CP1254Codec());
+  }
+
+  factory AnsiCodecs.cp1255() {
+    return AnsiCodecs._(const CP1255Codec());
+  }
+
+  factory AnsiCodecs.cp1256() {
+    return AnsiCodecs._(const CP1256Codec());
+  }
+}
+```
--- lib/src/ansiCodec_base.dart
+++ lib/src/ansiCodec_base.dart
@@ -16,94 +16,92 @@ import 'package:ansiCodec/src/cp949.dart';
 import 'package:ansiCodec/src/cp950.dart';
 
 class AnsiCodecs implements AnsiCodecSketch {
-   final AnsiCodecSketch codec;
-   
-   AnsiCodecs._(this.codec);
-   
-   factory AnsiCodecs.cp950(){
-      return AnsiCodecs._(const CP950Codec());
-   }
-   
-   factory AnsiCodecs.cp949(){
-      return AnsiCodecs._(const CP949Codec());
-   }
-   
-   factory AnsiCodecs.cp936(){
-      return AnsiCodecs._(const CP936Codec());
-   }
-   
-   factory AnsiCodecs.cp932(){
-      return AnsiCodecs._(const CP932Codec());
-   }
-   
-   factory AnsiCodecs.cp874(){
-      return AnsiCodecs._(const CP874Codec());
-   }
-   
-   factory AnsiCodecs.cp1250(){
-      return AnsiCodecs._(const CP1250Codec());
-   }
-   
-   factory AnsiCodecs.cp1251(){
-      return AnsiCodecs._(const CP1251Codec());
-   }
-   
-   factory AnsiCodecs.cp1252(){
-      return AnsiCodecs._(const CP1252Codec());
-   }
-   
-   factory AnsiCodecs.cp1253(){
-      return AnsiCodecs._(const CP1253Codec());
-   }
-   
-   factory AnsiCodecs.cp1254(){
-      return AnsiCodecs._(const CP1254Codec());
-   }
-   
-   factory AnsiCodecs.cp1255(){
-      return AnsiCodecs._(const CP1255Codec());
-   }
-   
-   factory AnsiCodecs.cp1256(){
-      return AnsiCodecs._(const CP1256Codec());
-   }
-   
-   
-   @override
-   String decode(List<int> encoded) {
-      return codec.decode(encoded);
-   }
-   
-   @override
-   // TODO: implement decoder
-   Converter<List<int>, String> get decoder => codec.decoder;
-   
-   @override
-   List<int> encode(String input) {
-      return codec.encode(input);
-   }
-   
-   @override
-   // TODO: implement encoder
-   Converter<String, List<int>> get encoder => codec.encoder;
-   
-   @override
-   Uint8List fromUnicodeToBytes(String input) {
-      return codec.fromUnicodeToBytes(input);
-   }
-   
-   @override
-   String fromUnicodeToHexString(String input) {
-      return codec.fromUnicodeToHexString(input);
-   }
-   
-   @override
-   Codec<String, R> fuse<R>(Codec<List<int>, R> other) {
+  final AnsiCodecSketch codec;
+
+  AnsiCodecs._(this.codec);
+
+  factory AnsiCodecs.cp950() {
+    return AnsiCodecs._(const CP950Codec());
+  }
+
+  factory AnsiCodecs.cp949() {
+    return AnsiCodecs._(const CP949Codec());
+  }
+
+  factory AnsiCodecs.cp936() {
+    return AnsiCodecs._(const CP936Codec());
+  }
+
+  factory AnsiCodecs.cp932() {
+    return AnsiCodecs._(const CP932Codec());
+  }
+
+  factory AnsiCodecs.cp874() {
+    return AnsiCodecs._(const CP874Codec());
+  }
+
+  factory AnsiCodecs.cp1250() {
+    return AnsiCodecs._(const CP1250Codec());
+  }
+
+  factory AnsiCodecs.cp1251() {
+    return AnsiCodecs._(const CP1251Codec());
+  }
+
+  factory AnsiCodecs.cp1252() {
+    return AnsiCodecs._(const CP1252Codec());
+  }
+
+  factory AnsiCodecs.cp1253() {
+    return AnsiCodecs._(const CP1253Codec());
+  }
+
+  factory AnsiCodecs.cp1254() {
+    return AnsiCodecs._(const CP1254Codec());
+  }
+
+  factory AnsiCodecs.cp1255() {
+    return AnsiCodecs._(const CP1255Codec());
+  }
+
+  factory AnsiCodecs.cp1256() {
+    return AnsiCodecs._(const CP1256Codec());
+  }
+
+  @override
+  String decode(List<int> encoded) {
+    return codec.decode(encoded);
+  }
+
+  @override
+  // TODO: implement decoder
+  Converter<List<int>, String> get decoder => codec.decoder;
+
+  @override
+  List<int> encode(String input) {
+    return codec.encode(input);
+  }
+
+  @override
+  // TODO: implement encoder
+  Converter<String, List<int>> get encoder => codec.encoder;
+
+  @override
+  Uint8List fromUnicodeToBytes(String input) {
+    return codec.fromUnicodeToBytes(input);
+  }
+
+  @override
+  String fromUnicodeToHexString(String input) {
+    return codec.fromUnicodeToHexString(input);
+  }
+
+  @override
+  Codec<String, R> fuse<R>(Codec<List<int>, R> other) {
+    throw Exception('not implemented yet');
+  }
+
+  @override
+  Codec<List<int>, String> get inverted =>
       throw Exception('not implemented yet');
-   }
-   
-   @override
-   Codec<List<int>, String> get inverted => throw Exception('not implemented yet');
-   
-   
 }
